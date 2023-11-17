953adb3ed4720cca2e927bd3b479f9fc29e266aa
Mans Ramazanov <Mans-606@yandex.ru>
2023-11-17 16:47:07 +0300

    initial commit create app add tailwind and typography

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container mx-auto bg-gray-200 rounded-xl">
+    <p className="text-gray-500 text-lg">Hello</p>
+    </div>
+  );
+}
+
+export default App;
